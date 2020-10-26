effb254f6f9978ba26c05f492867f789dbd4818b
KyungminUm <snownaul2@gmail.com>
2020-10-26 16:06:26 -0400

    - Record, Block

--- src/com/kyungminum/Block.java
+++ src/com/kyungminum/Block.java
@@ -1,4 +1,46 @@
 package com.kyungminum;
 
+import java.io.RandomAccessFile;
+
 public class Block {
+    private Record record;
+    private int next;
+    private int prev;
+
+    Block(){
+        record = new Record();
+
+    }
+
+    public void read(RandomAccessFile f){
+        record.read(f);
+    }
+
+    public void write(RandomAccessFile f){
+        record.write(f);
+    }
+
+    public Record getRecord() {
+        return record;
+    }
+
+    public void setRecord(Record record) {
+        this.record = record;
+    }
+
+    public int getNext() {
+        return next;
+    }
+
+    public void setNext(int next) {
+        this.next = next;
+    }
+
+    public int getPrev() {
+        return prev;
+    }
+
+    public void setPrev(int prev) {
+        this.prev = prev;
+    }
 }
--- src/com/kyungminum/Record.java
+++ src/com/kyungminum/Record.java
@@ -14,10 +14,46 @@ public class Record {
         accountNumber = -1;
         firstName = new char[15];
         lastName = new char[15];
+        balance = 0;
     }
 
     public void read(RandomAccessFile f){
 
     }
 
+    public void write(RandomAccessFile f){
+
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public char[] getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(char[] firstName) {
+        this.firstName = firstName;
+    }
+
+    public char[] getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(char[] lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
