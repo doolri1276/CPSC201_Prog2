b418477078dc9523f520b43b0aedc893173d6a79
jangkunsuguri <jsuguri@my.bridgeport.edu>
2020-11-02 02:49:33 -0500

    no message

--- src/com/kyungminum/ProcessFile.java
+++ src/com/kyungminum/ProcessFile.java
@@ -414,10 +414,13 @@ public class ProcessFile {
         }
         b.read(f);
         r = b.getRecord();
-        while(r.getAccountNumber()<acct){
+        while(r.getAccountNumber()<=acct){
             cur = b.getNext();
             if (cur == -1){
                 break;
+            }
+            if(r.getAccountNumber() == acct){
+                return cur;
             }
             try {
                 f.seek(cur);
